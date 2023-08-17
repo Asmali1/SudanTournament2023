041f3cf1f04c6094876c5bc97c2f2feea7b1096a
Ahmed Abdelwahab <ahmedua2003@gmail.com>
2023-08-16 23:06:50 -0400

    about us

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function displayPrayerTimesForDay(index, data) {
             const minutes = Math.floor(diff / (1000 * 60));
             diff %= (1000 * 60);
             const seconds = Math.floor(diff / 1000);
-            document.getElementById("countdown").innerText = `Next ${nextPrayerName}: ${formatWithLeadingZero(hours)}:${formatWithLeadingZero(minutes)}:${formatWithLeadingZero(seconds)}`;
+            document.getElementById("prayer-countdown").innerText = `Next ${nextPrayerName}: ${formatWithLeadingZero(hours)}:${formatWithLeadingZero(minutes)}:${formatWithLeadingZero(seconds)}`;
         }, 1000);
     }
     
@@ -136,7 +136,30 @@ fetch(apiURL)
                 const minutes = Math.floor(diff / (1000 * 60));
                 diff %= (1000 * 60);
                 const seconds = Math.floor(diff / 1000);
-                document.getElementById("countdown").innerText = `Next ${nextPrayerName}: ${formatWithLeadingZero(hours)}:${formatWithLeadingZero(minutes)}:${formatWithLeadingZero(seconds)}`;
+                document.getElementById("prayer-countdown").innerText = `Next ${nextPrayerName}: ${formatWithLeadingZero(hours)}:${formatWithLeadingZero(minutes)}:${formatWithLeadingZero(seconds)}`;
             }
         }, 1000);
     }
+
+    
+// Set the target date for the countdown
+const targetDate = new Date("2023-09-01T23:59:59").getTime();
+
+// Update the countdown every second
+const countdown = setInterval(function() {
+  const now = new Date().getTime();
+  const timeRemaining = targetDate - now;
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
+
+  // If the countdown is over, display a message
+  if (timeRemaining < 0) {
+    clearInterval(countdown);
+    document.getElementById("timer").innerHTML = "Countdown expired!";
+  }
+}, 1000);
