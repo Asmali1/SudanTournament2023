603701461daf9ac133ac5a1b23d3bac348ad3228
Ahmed Abdelwahab <ahmedua2003@gmail.com>
2023-08-21 23:53:57 -0400

    updated version

--- AboutUs/script.js
+++ AboutUs/script.js
@@ -1,27 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const pdfViewer = document.getElementById("pdfViewer");
-    const prevButton = document.getElementById("prevButton");
-    const nextButton = document.getElementById("nextButton");
-  
-    let currentPage = 1;
-    const totalPages = pdfViewer.contentDocument ? pdfViewer.contentDocument.body.children.length : 0;
-  
-    function goToPage(pageNumber) {
-      if (pageNumber >= 1 && pageNumber <= totalPages) {
-        pdfViewer.contentWindow.scrollTo(0, pdfViewer.contentDocument.body.children[pageNumber - 1].offsetTop);
-        currentPage = pageNumber;
-      }
-    }
-  
-    prevButton.addEventListener("click", function () {
-      goToPage(currentPage - 1);
-    });
-  
-    nextButton.addEventListener("click", function () {
-      goToPage(currentPage + 1);
-    });
-  });
-  
 
   function toggleSidePanel() {
     const sidePanel = document.getElementById('sidePanel');
