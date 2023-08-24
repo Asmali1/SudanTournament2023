c51a33cb0a88aeeac49d700e6ac2e41c0b4d044b
Mohamed Asmali <mohamedasmali@hotmail.com>
2023-08-24 09:39:06 -0400

    added yelp food relocation on about us page

--- AboutUs/script.js
+++ AboutUs/script.js
@@ -1,4 +1,29 @@
 
+  // Utility function to format a single digit number with a leading zero
+function formatWithLeadingZero(number) {
+    return number < 10 ? '0' + number : number;
+}
+
+function redirectToYelp() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            window.location.href = `https://www.yelp.com/search?find_desc=Halal&find_loc=${latitude},${longitude}`;
+        }, function(error) {
+            // Geolocation failed for some reason, use default address
+            redirectToDefaultAddress();
+        });
+    } else {
+        // Geolocation not supported, use default address
+        redirectToDefaultAddress();
+    }
+}
+
+function redirectToDefaultAddress() {
+    const defaultAddress = "6490 Dublin Park Drive";
+    window.location.href = `https://www.yelp.com/search?find_desc=Halal&find_loc=${encodeURIComponent(defaultAddress)}`;
+}
   function toggleSidePanel() {
     const sidePanel = document.getElementById('sidePanel');
     const toggleIcon = document.getElementById('toggleBtn');
